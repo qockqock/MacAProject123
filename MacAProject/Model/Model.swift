726d65cff8671ee048cf953803cc62beeceecf91
daesung <eogud8203@naver.com>
2024-07-02 14:55:30 +0900

    기본 세팅

---
+++ MacAProject/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  MacAProject
+//
+//  Created by 머성이 on 7/2/24.
+//
+
+import Foundation
---
+++ MacAProject/View/CustomMainView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomMainView.swift
+//  MacAProject
+//
+//  Created by 머성이 on 7/2/24.
+//
+
+import UIKit
+
+class CustomMainView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
